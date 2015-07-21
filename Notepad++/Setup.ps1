3a127706976ba14b5b549f14086a63f546f0efdd
Ben Origas <borigas@nebraskaglobal.com>
2015-07-21 17:16:21 -0500

    Added script to copy notepad++ profile

---
+++ Notepad++/Setup.ps1
@@ -0,0 +1,11 @@
+# https://github.com/SeanCline/Npp-VS2012-Dark
+# Installation (per-machine)
+
+####  Actually goes to APPDATA  #####
+# Download VS2012-Dark.xml, making sure to get the raw version, to %PROGRAMFILES%\Notepad++\themes (%PROGRAMFILES(X86)%\Notepad++\themes on 64-bit systems).
+# Restart Notepad++.
+# Open Settings->Style Configurator.
+# Select VS2012-Dark from the theme drop-down box.
+# Click Save & Close
+
+cp "VS2012-Dark.xml" ($env:APPDATA + "\Notepad++\themes\")
