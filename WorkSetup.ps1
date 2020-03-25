993fd907da0a27b92ffb6bf48f8664844e68a7c6
Ben Origas <borigas@ocuvera.com>
2020-03-25 11:50:18 -0500

    A couple more work tools

--- WorkSetup.ps1
+++ WorkSetup.ps1
@@ -7,4 +7,6 @@ choco install office365business -y
 choco install sql-server-management-studio -y
 choco install sql-server-express -y
 choco install visualstudio2019enterprise -y --config ./work.vsconfig
+choco install microsoftazurestorageexplorer -y
+choco install servicebusexplorer -y
 choco install todobackup -y
