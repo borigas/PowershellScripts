6cfd5792f645aec298f32afec534ad9ea6b6ed69
Ben Origas <borigas@ocuvera.com>
2022-03-23 15:13:52 -0500

    Add kube_ps1, krew, kubectx, and kubens

--- Linux/Profile/profile.sh
+++ Linux/Profile/profile.sh
@@ -19,3 +19,11 @@ alias gs='git status'
 # Change to Windows style tab completion (https://superuser.com/questions/59175/is-there-a-way-to-make-bash-more-tab-friendly)
 bind '"\t":menu-complete'
 bind '"\e[Z":menu-complete-backward'
+
+# Change prompt to include kubectl context
+source ~/profile/kube-ps1.sh
+KUBE_PS1_SYMBOL_ENABLE=false
+export PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] $(kube_ps1)\$ '
+
+# Add Krew to the path
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
